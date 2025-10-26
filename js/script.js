742d677b63cfc9f7c0aeeab877c1d1ee6f8d6557
MoOpVII <156279600+MoOpVII@users.noreply.github.com>
2025-10-26 15:56:21 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,5 @@
-/*Name this external file gallery.js*/
-
 function upDate(previewPic){
- /* In this function you should
-    1) change the url for the background image of the div with the id = "image"
-    to the source file of the preview image
-
-    2) Change the text  of the div with the id = "image"
-    to the alt text of the preview image
-    */
+	
     console.log("upDate triggered");
     console.log("previewPic.src:", previewPic.src);
     console.log("previewPic.alt:", previewPic.alt);
@@ -17,13 +9,7 @@ function upDate(previewPic){
 	}
 
 	function unDo(){
-     /* In this function you should
-    1) Update the url for the background image of the div with the id = "image"
-    back to the orginal-image.  You can use the css code to see what that original URL was
 
-    2) Change the text  of the div with the id = "image"
-    back to the original text.  You can use the html code to see what that original text was
-    */
     console.log("unDo triggered");
 
     document.getElementById('image').style.backgroundImage = "url('')";
